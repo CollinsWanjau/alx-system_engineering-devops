7d537385da9b53b4a56d8179232d8dd7ba12fde4
colloso999 <tripperskripper@gmail.com>
2023-08-08 23:22:23 +0300

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return (0)
 
-    user_agent = {'User-agent': 'Google Chrome Version 111.0.5563.64'}
+    user_agent = {'User-agent': 'Google Chrome Version 114.0.5735.198'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
